23e092dc6d5d9e5bf28e80080f026af674e05abf
Erick Garcia <ericksoft597@gmail.com>
2019-09-21 17:13:39 -0500

    configuracion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {config} from 'rxjs';
+import {NotFoundComponent} from './shared/not-found/not-found.component';
+import {TemplateComponent} from './thema/template/template.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: './auth/auth.module#AuthModule'
+  },
+  {path: 'Pages',
+    component: TemplateComponent,
+  children: [
+    {
+      path: 'biblioteca',
+      loadChildren: './Pages/biblioteca/biblioteca.module#BibliotecaModule'
+    }]},
+  {path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
